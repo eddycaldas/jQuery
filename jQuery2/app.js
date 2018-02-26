dfbe24b4aaf96c3c7279afe1d0065154a96c7773
Eddy Caldas <eddycaldas@live.com>
2018-02-26 09:35:43 -0700

    slideToggle

--- jQuery2/app.js
+++ jQuery2/app.js
@@ -1,7 +1,18 @@
+// $(document).ready(function() {
+//   $('#menu-toggle').click(function() {
+//     $('#side-nav').toggle(1000, function() {
+//       alert('Hi there');
+//     });
+//   })
+// })
+// 
 $(document).ready(function() {
   $('#menu-toggle').click(function() {
-    $('#side-nav').toggle(1000, function() {
-      alert('Hi there');
-    });
+    $('#side-nav').toggle(1000);
   })
+
+  $('.info-title').click(function() {
+    $('.info-content').slideToggle();
+});
+
 })
