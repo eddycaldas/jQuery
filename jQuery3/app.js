b8496c584ede0dcf6d89117bbcfe7b0f156512c3
Eddy Caldas <eddycaldas@live.com>
2018-02-26 20:51:04 -0700

    animate

--- jQuery3/app.js
+++ jQuery3/app.js
@@ -1,10 +1,12 @@
 
 $(document).ready(function() {
+  
   $(".content-box").click(function () {
     $(".content-box").animate( {
       width: '500px',
-      height: '+=200px'
-    }, 1000 );
-    $('.content-after').show().animate( { opacity: 1 }, 3000 )
+      height: '400px'
+    }, 3000 ).animate({
+      fontSize: '30px'
+    }, 3000 ).fadeOut();
   });
 });
