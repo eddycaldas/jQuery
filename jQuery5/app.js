579d91f5073f0551b052d5618224ed667c807590
Eddy Caldas <eddycaldas@live.com>
2018-02-26 21:11:03 -0700

    DOM with jQuery

---
+++ jQuery5/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  
+  $("#btn1").click(function() {
+    alert($("#test1").text());
+  });
+  
+  $("#btn2").click(function() {
+    alert($("#test2").html());
+  });
+  
+  $("#btn3").click(function() {
+    alert($("#test3").val());
+  });
+  
+  
+});
